ba2674be416fcbfb2e626fcaf0945e5159b9ef48
Gautam Sharma <gautamsh@leewayhertz.com>
2023-04-17 09:55:59 +0530

    changes

--- back-end/router/index.js
+++ back-end/router/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controller/auth");
-const { updateUser ,getUser ,getAllUser,deleteUser,stats} = require("../controller/user");
+const { updateUser, getUser, getAllUser, deleteUser, stats } = require("../controller/user");
 const { verifyToken } = require("../middleware");
 
 //auth
 router.post("/api/auth/register", register);
 router.post("/api/auth/login", login);
-
 //user-routes
 router.get("/api/user/:id", getUser);
 router.get("/api/user",verifyToken,getAllUser);
