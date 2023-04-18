1f679d0715181478407a9bd76b1622b338322cb7
gautam sharma <gautamsharma.gs48@gmail.com>
2023-04-18 21:11:25 +0530

    ui fixes

---
+++ back-end/controller/movie.js
@@ -0,0 +1,79 @@
+const movieSchema = require("../models/movieModel");
+
+const createMovie = async (req, res) => {
+  if (req.user.isAdmin) {
+    const newMovie = new Movie();
+    try {
+      const savedMovie = await newMovie.save();
+      res.status(201).json(savedMovie);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+};
+
+const updateMovie = async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const updateMovie = await movieSchema.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updateMovie);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+};
+
+const deleteMovie = async () => {
+  if (req.user.isAdmin) {
+    try {
+      const deleteMovie = await movieSchema.findByIdAndDelete(req.params.id);
+      res.status(200).json(deleteMovie);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+};
+
+const getMovie = async () => {
+  try {
+    const movie = await movieSchema.findById(req.params.id);
+    res.status(200).json(movie);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const getAllMovie = async (req, res) => {
+  try {
+    const { page = 1, limit = 10, userLimit = false } = req.query;
+    const movies = userLimit
+      ? await movieSchema
+          .find()
+          .skip((page - 1) * limit)
+          .limit(limit * 1)
+      : await movieSchema.find();
+    res.status(200).json(movies.reverse());
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+const randomMovie = () => {};
+
+module.exports = {
+  createMovie,
+  updateMovie,
+  deleteMovie,
+  randomMovie,
+  getMovie,
+  getAllMovie,
+};
--- back-end/router/index.js
+++ back-end/router/index.js
@@ -2,18 +2,27 @@ const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controller/auth");
 const { updateUser, getUser, getAllUser, deleteUser, stats } = require("../controller/user");
+const { createMovie ,updateMovie , deleteMovie, getMovie, getAllMovie }=require("../controller/movie");
 const { verifyToken } = require("../middleware");
+
 
 //auth
 router.post("/api/auth/register", register);
 router.post("/api/auth/login", login);
+
 //user-routes
 router.get("/api/user/:id", getUser);
 router.get("/api/user",verifyToken,getAllUser);
 router.put("/api/user/:id",verifyToken, updateUser);
 router.delete("/api/user/:id",verifyToken, deleteUser);
 router.get("/api/user/stats",stats);
+
 //movies-routes
+router.get("/api/movie",getAllMovie);
+router.get("/api/movie/:id",getMovie);
+router.post("/api/movie",verifyToken,createMovie);
+router.put("/api/movie/:id",verifyToken,updateMovie);
+router.delete("/api/movie/:id",verifyToken,deleteMovie);
 
 //list-routes
 
